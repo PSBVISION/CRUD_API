e801ca0d8bb9c12be5a481f119bb88f5e614cbb3
PUNIT SINGH BISHT <punitsinghbisht1529@gmail.com>
2024-03-07 22:22:51 +0530

    added product model

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
 
 const app = express();
+
+app.use(express.json());
+
 
 
 
 app.get('/', (req, res) => {
     res.send('Hello from node API badmash! ');
+});
+
+
+app.post('/api/products', async(req, res) => {
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json( product );
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
 });
 
 mongoose.connect("mongodb+srv://admin:9Y6aNJTHrkCqmeYf@cluster0.yf6gjbd.mongodb.net/Node-API?retryWrites=true&w=majority")
