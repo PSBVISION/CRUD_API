909e8b9493639f90bb3ac7e103c17cb91425db7e
PUNIT SINGH BISHT <punitsinghbisht1529@gmail.com>
2024-03-08 21:37:23 +0530

    some changes

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.get('/', (req, res) => {
 app.post('/api/products', async(req, res) => {
     try {
         const product = await Product.create(req.body);
-        res.status(200).json( product );
+        res.status(200).json( product);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
